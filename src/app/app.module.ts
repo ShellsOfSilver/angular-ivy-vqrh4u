13a10e7f1d1b696c49f411ce732cfe08c15da191
ShellsOfSilver <shellsofsilver13@gmail.com>
2021-01-30 09:43:13 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
+import { MatSliderModule } from "@angular/material/slider";
+import { MatButtonModule } from "@angular/material/button";
+import { MatStepperModule } from "@angular/material/stepper";
+
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatStepperModule
+  ],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
